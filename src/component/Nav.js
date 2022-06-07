0ed499d09da890b9c00164ab2323fb5cf1b3a5d3
Boluwatife Ajibola <ajibolss11@gmail.com>
2022-06-07 17:50:03 -0400

    added a nav bar component

---
+++ src/component/Nav.js
@@ -0,0 +1,12 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+function Nav(props) {
+  return (
+    <Box sx={{ backgroundColor: "#cd452b", color: "#fff", paddingLeft: 1 }}>
+      <Typography variant="h3">Timer</Typography>
+    </Box>
+  );
+}
+
+export default Nav;
