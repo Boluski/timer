4dd831d83cae8d556fbda9e5c25335d8259e16ff
Boluwatife Ajibola <ajibolss11@gmail.com>
2022-06-08 14:28:14 -0400

    finished the add button component design

--- src/component/AddButton.js
+++ src/component/AddButton.js
@@ -1,18 +1,28 @@
 import React from "react";
-
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 
+const theme = createTheme({
+  palette: { pop: { main: "#cd452b", contrastText: "#fff" } },
+});
+
 function AddButton() {
   return (
     <Grid item xs={3}>
-      <Button
-        variant="outlined"
-        sx={{ backgroundColor: "#cd452b", width: "100%", height: "16rem" }}
-      >
-        <AddIcon sx={{ color: "#ffffff", fontSize: "6rem" }}></AddIcon>
-      </Button>
+      <ThemeProvider theme={theme}>
+        <Button
+          color="pop"
+          variant="outlined"
+          sx={{
+            width: "100%",
+            height: "16rem",
+          }}
+        >
+          <AddIcon sx={{ fontSize: "6rem" }}></AddIcon>
+        </Button>
+      </ThemeProvider>
     </Grid>
   );
 }
